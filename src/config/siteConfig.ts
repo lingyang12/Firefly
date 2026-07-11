6fe7ad25d6704c3ee69a4048cf284f7256e625fd
lingyang_12 <lingyang_12@qq.com>
2026-07-11 10:43:22 +0800

    chore: 更新配置文件和赞助图片

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,17 +6,17 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "汐汐的窝",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "爱总是短暂的,如果认真对待极为完善",
 
 	// 站点 URL
 	site_url: "https://firefly.cuteleaf.cn",
 
 	// 站点描述
 	description:
-		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"汐汐是一个善良的人，这里将会给你带来更多的知识，技术以及教程喵！",
 
 	// 站点关键词
 	keywords: [
@@ -27,6 +27,7 @@ export const siteConfig: SiteConfig = {
 		"博客",
 		"技术博客",
 		"静态博客",
+
 	],
 
 	// 主题色
@@ -90,7 +91,7 @@ export const siteConfig: SiteConfig = {
 	},
 
 	// 站点开始日期，用于统计运行天数
-	siteStartDate: "2025-01-01",
+	siteStartDate: "2026-07-06",
 
 	// 站点时区（IANA 时区字符串），用于格式化bangumi、rss里的构建日期时间等等..
 	// 示例："Asia/Shanghai", "UTC", 如果为空，则按照构建服务器的时区进行时区转换
@@ -211,7 +212,7 @@ export const siteConfig: SiteConfig = {
 		// Bilibili 配置
 		bilibili: {
 			// 你的 Bilibili 用户 UID
-			uid: "38932988",
+			uid: "3546554455558263",
 		},
 		// TMDB 配置（可选，需要翻墙）
 		// tmdb: {
